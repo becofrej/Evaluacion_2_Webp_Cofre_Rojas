784f93142455860048746689c3e79c3e0e8288b7
David Rojas <indior.dev@gmail.com>
2024-06-24 01:08:19 -0400

    Se importaron las urls de autenticación

--- PawPalace/urls.py
+++ PawPalace/urls.py
@@ -20,9 +20,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('productos.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
 ]
 
 if settings.DEBUG:
