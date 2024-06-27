1e5e0ac4fb8500606d7989658390645575c17788
David Rojas <indior.dev@gmail.com>
2024-06-27 07:53:09 -0400

    se añadieron validaciones de campos para agregar_productos

--- productos/forms.py
+++ productos/forms.py
@@ -11,6 +11,12 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
 
 class ProductoForm(forms.ModelForm):
+
+    nombre = forms.CharField(min_length=3, max_length=50)
+    imagen = forms.ImageField(required=False)
+    precio = forms.IntegerField(min_value=1, max_value=1500000)
+    stock = forms.IntegerField(min_value=1)
+
     class Meta:
         model = Producto
         fields = '__all__'
