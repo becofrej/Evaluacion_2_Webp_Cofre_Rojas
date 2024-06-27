d61ad18a33e3373a48f8e5f7a3a9a0cfa3ac55cc
David Rojas <indior.dev@gmail.com>
2024-06-27 04:38:45 -0400

    se configuró la ruta para agregar y listar producto

--- productos/urls.py
+++ productos/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import home, pagPerros, pagGatos, pagAccesorios, pagSnacks, pagIngresar, pagRegistrar, pagCarrito
+from .views import home, pagPerros, pagGatos, pagAccesorios, pagSnacks, pagIngresar, pagRegistrar, pagCarrito, agregar_producto, listar_producto, CustomLoginView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -13,6 +13,6 @@ urlpatterns = [
     path('registrarse/', views.pagRegistrar, name='registrarse'),
     path('carrito/', pagCarrito, name="pagCarrito"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
-    
+    path('agregar-producto/', agregar_producto, name="agregar_producto"),
+    path('listar-producto/', listar_producto, name="listar_producto")
 ]
