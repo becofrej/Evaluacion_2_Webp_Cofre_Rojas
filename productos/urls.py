6189d54b86e773a47b177fad0783be810326e5fe
David Rojas <indior.dev@gmail.com>
2024-06-27 05:53:31 -0400

    se configuraron nuevas rutas
    se implementaron las URLS de listar, modificar y eliminar productos

--- productos/urls.py
+++ productos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-from .views import home, pagPerros, pagGatos, pagAccesorios, pagSnacks, pagIngresar, pagRegistrar, pagCarrito, agregar_producto, listar_producto
+from .views import home, pagPerros, pagGatos, pagAccesorios, pagSnacks, pagIngresar, pagRegistrar,\
+    pagCarrito, agregar_producto, listar_producto, modificar_producto, eliminar_producto
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -14,5 +15,7 @@ urlpatterns = [
     path('carrito/', pagCarrito, name="pagCarrito"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('agregar-producto/', agregar_producto, name="agregar_producto"),
-    path('listar-producto/', listar_producto, name="listar_producto")
+    path('listar-producto/', listar_producto, name="listar_producto"),
+    path('modificar-producto/<id>/', modificar_producto, name="modificar_producto"),
+    path('eliminar-producto/<id>/', eliminar_producto, name="eliminar_producto"),
 ]
