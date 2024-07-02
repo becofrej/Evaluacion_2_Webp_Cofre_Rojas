ae4834d1fc0ceddb4d76fbb1ef7fe80992cc3baf
Bcofrex <be.cofrej@duocuc.cl>
2024-07-02 19:19:12 -0400

    cambios en databases en settings

--- PawPalace/settings.py
+++ PawPalace/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'PawPalace.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
 }
